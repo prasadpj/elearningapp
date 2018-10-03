4d10d8ed69caa2577f158a3dacd37d16dacc60a3
manish <manishchavan016@gmail.com>
2018-10-03 10:56:01 +0530

    correct put method to post

--- server/api/user/user.api.js
+++ server/api/user/user.api.js
@@ -7,6 +7,6 @@ Router.get('/', Controller.readAll);
 Router.get('/:id', Controller.read);
 Router.post('/', Controller.create);
 Router.put('/', Controller.update);
-Router.put('/byEmail', Controller.readByEmailId);
+Router.post('/byEmail', Controller.readByEmailId);
 
 module.exports = Router;
