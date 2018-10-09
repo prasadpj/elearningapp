061c395f2eca2caf8b573efde7dce8e4f8f33984
Prasad Jagdale <prasad@forkmedia.in>
2018-10-09 23:02:48 +0530

    hide footer element based on url string

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ElearningApp';
+  hideFooter = false;
+  constructor(
+    private router: Router,
+  ) {
+    this.router.events.subscribe(() => {
+      // console.log('this.router.url ', this.router.url)
+      // if url contains home hide footer
+      if (this.router.url.indexOf('home') > -1) {
+        this.hideFooter = true
+      }else{
+        this.hideFooter = false
+      }
+    })
+
+  }
 }
--- client/app/components/client-footer/client-footer.component.ts
+++ client/app/components/client-footer/client-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-client-footer',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClientFooterComponent implements OnInit {
 
   constructor() { }
-
+  @Input() hideFooter: boolean;
   ngOnInit() {
   }
 
