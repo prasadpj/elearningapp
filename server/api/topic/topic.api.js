d3fdab241197f573c2480aa046dc14788893563e
Prasad Jagdale <prasad@forkmedia.in>
2018-10-17 14:15:55 +0530

    added middleware auth for topic api

--- server/api/topic/topic.api.js
+++ server/api/topic/topic.api.js
@@ -4,12 +4,17 @@ var ApiMiddleware = appRequire('service.api.auth');
 var Router = Express.Router();
 
 
-Router.use('/', ApiMiddleware.restrictAPI)
 Router.get('/', Controller.readAll);
 Router.get('/byChapterID/:id', Controller.readByChapterId);
 Router.get('/:id', Controller.read);
+
+Router.post('/', ApiMiddleware.restrictAPI)
 Router.post('/', Controller.create);
+
+Router.put('/', ApiMiddleware.restrictAPI)
 Router.put('/:id', Controller.update);
+
+Router.delete('/', ApiMiddleware.restrictAPI)
 Router.delete('/:id', Controller.delete);
 
 module.exports = Router;
--- server/service/api.auth.middleware.js
+++ server/service/api.auth.middleware.js
@@ -14,7 +14,8 @@ function restrictAPI(req, res, next) {
         return res.status(401).send({ auth: false, message: 'No token provided.' });
 
     jwt.verify(token, config.EncryptionKey, function (err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        if (err)
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
         next()
     });
 }
